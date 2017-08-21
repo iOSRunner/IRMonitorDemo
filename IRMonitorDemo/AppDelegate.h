fb7a089bd6b6069e9e4bb49b43442d7d732ef06a
文楷 <wanlei.gwl@alibaba-inc.com>
2017-08-21 15:39:21 +0800

    init Project

---
+++ IRMonitorDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IRMonitorDemo
+//
+//  Created by Wanlei on 2017/8/21.
+//  Copyright © 2017年 CaiNiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IRMonitorDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IRMonitorDemo
+//
+//  Created by Wanlei on 2017/8/21.
+//  Copyright © 2017年 CaiNiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
